938b810bdaeb6e4c56f9e1b792fcf266c156a7da
BENDI SAIKUMAR <s27k02b2003@gmail.com>
2025-07-05 13:23:43 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -43,8 +43,8 @@ export const Hero = () => {
           <div className="relative">
   <div className="w-48 h-48 mx-auto rounded-full overflow-hidden border-4 border-white shadow-blue-lg">
     <img
-      src={`${process.env.PUBLIC_URL}/saikumar.jpg`}
-      alt="Profile photo"
+      src={'@/src/saikumar.jpg'}
+      alt="Profilephoto"
       className="w-full h-full object-cover"
     />
   </div>
