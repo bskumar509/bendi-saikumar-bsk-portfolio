b27c988952d97b3b72ed97f0959d62e05acef2ac
BENDI SAIKUMAR <s27k02b2003@gmail.com>
2025-07-05 13:17:26 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,6 +2,7 @@
 import { Button } from "@/components/ui/button";
 import { Mail, Github, Linkedin, Download, ArrowDown } from "lucide-react";
 import { useEffect, useState } from "react";
+import Profilephoto from '@/pages/saikumar.jpg';
 
 export const Hero = () => {
   const [displayText, setDisplayText] = useState("");
@@ -43,8 +44,8 @@ export const Hero = () => {
           <div className="relative">
   <div className="w-48 h-48 mx-auto rounded-full overflow-hidden border-4 border-white shadow-blue-lg">
     <img
-      src={`${process.env.PUBLIC_URL}/saikumar.jpg`}
-      alt="Profile photo"
+      src={process.env.src/saikumar.jpg}
+      alt="Profilephoto"
       className="w-full h-full object-cover"
     />
   </div>
